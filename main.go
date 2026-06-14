bb162017e23e55f87664a6e6fae4d08d2229922d
4codegit <4codegit@users.noreply.github.com>
2026-06-14 21:00:21 +0500

    fix: bundle Mesa OpenGL for Windows release

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 	if runtime.GOOS == "windows" && os.Getenv("FYNE_RENDER") == "" {
 		os.Setenv("FYNE_RENDER", "software")
 	}
+	if runtime.GOOS == "windows" && os.Getenv("GALLIUM_DRIVER") == "" {
+		os.Setenv("GALLIUM_DRIVER", "llvmpipe")
+	}
+	if runtime.GOOS == "windows" && os.Getenv("LIBGL_ALWAYS_SOFTWARE") == "" {
+		os.Setenv("LIBGL_ALWAYS_SOFTWARE", "1")
+	}
 
 	controller := NewAppController()
 	controller.Run()
